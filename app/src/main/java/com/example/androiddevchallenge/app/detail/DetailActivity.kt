bdc6a5f8852c61589b6a4283fde4c0eabce798a0
Wavky.Huang <master@wavky.com>
2021-03-02 06:09:25 +0900

    fix format

--- app/src/main/java/com/example/androiddevchallenge/app/detail/DetailActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/app/detail/DetailActivity.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.app.detail
 
 import android.content.Intent
@@ -52,7 +67,8 @@ class DetailActivity : AppCompatActivity() {
                                         .align(Alignment.BottomEnd)
                                         .padding(16.dp),
                                     onClick = {
-                                    })
+                                    }
+                                )
                             }
                         }
                     }
@@ -76,7 +92,8 @@ private fun Preview() {
                         modifier = Modifier
                             .align(Alignment.BottomEnd)
                             .padding(16.dp),
-                        onClick = {})
+                        onClick = {}
+                    )
                 }
             }
         }
